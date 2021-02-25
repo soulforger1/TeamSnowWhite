d40c4f95188d702997b206e003f9ab75cbafbd50
ermuun10 <ermuunee10@gmail.com>
2021-02-25 15:44:19 +0800

    feat: button

--- src/screens/cart.tsx
+++ src/screens/cart.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import { Button } from '../components';
 
 export const Cart = () => {
-  return <View style={styles.container}></View>;
+  return(
+    <View style={styles.container}>
+      <Button width={364} height={67} borderRadius={19} text='Go To Checkout'/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
