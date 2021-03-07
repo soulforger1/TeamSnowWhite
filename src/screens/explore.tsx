d52a08bf0ad22eb64b3c6caa54337b366e932fb0
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2021-03-07 21:59:03 +0800

    feat: explore screen

--- src/screens/explore.tsx
+++ src/screens/explore.tsx
@@ -1,14 +1,84 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Search} from '../components';
+import {useFireStoreCol} from '../hooks';
+
+const Card = ({color, poster, name}: any) => {
+  return (
+    <TouchableOpacity>
+      <View style={[styles.cardMain, {borderColor: color}]}>
+        <View style={[styles.cardBackground, {backgroundColor: color}]} />
+        <Image source={{uri: poster}} style={styles.cardPoster} />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export const Explore = () => {
-  return <View style={styles.container}></View>;
+  const categories = useFireStoreCol('categories').collection;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Find Product</Text>
+      <Search />
+      <FlatList
+        style={{paddingTop: 20}}
+        data={categories}
+        renderItem={({item, index}) => (
+          <Card color={item.color} poster={item.poster} name={item.name} />
+        )}
+        keyExtractor={(item) => item.name}
+        horizontal={false}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    height: 50,
+    fontSize: 25,
+    fontWeight: '600',
+  },
+  cardMain: {
+    margin: 7.5,
+    height: 190,
+    width: 175,
+    borderWidth: 2,
+    borderRadius: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardBackground: {
+    position: 'absolute',
+    width: 175,
+    height: 190,
+    opacity: 0.2,
+    borderRadius: 18,
+  },
+  cardPoster: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+  },
+  name: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: '500',
   },
 });
